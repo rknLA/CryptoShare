ccff9cc3974c5d24105d4f78484b40758c2101d5
R. Kevin Nelson <kevin@rkn.la>
2015-01-25 15:50:18 +0100

    Restructure directories, add create password vc

---
+++ CryptoShare/View Controllers/CreatePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreatePasswordViewController.h
+//  CryptoShare
+//
+//  Created by Kevin Nelson on 1/25/15.
+//  Copyright (c) 2015 rknLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreatePasswordViewController : UIViewController
+
+@end
---
+++ CryptoShare/View Controllers/CreatePasswordViewController.m
@@ -0,0 +1,13 @@
+//
+//  CreatePasswordViewController.m
+//  CryptoShare
+//
+//  Created by Kevin Nelson on 1/25/15.
+//  Copyright (c) 2015 rknLA. All rights reserved.
+//
+
+#import "CreatePasswordViewController.h"
+
+@implementation CreatePasswordViewController
+
+@end
